a8e1c9e69ba01131753874cedc51ffc2c6fd9e5c
GAMING <stu2201321016@uni-plovdiv.bg>
2026-05-21 09:57:49 +0300

    Model Updates

--- School Warehouse/Models/Item.cs
+++ School Warehouse/Models/Item.cs
@@ -3,5 +3,6 @@ public class Item
     public int Id { get; set; }
     public string Name { get; set; }
     public string Category { get; set; }
+    public string Location {  get; set; } = string.Empty;
     public int Quantity { get; set; }
 }
