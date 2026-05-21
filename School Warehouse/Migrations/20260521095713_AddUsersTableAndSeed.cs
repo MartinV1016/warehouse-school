3ed4bec52dd2ebf45dd53054b8fc4b3678adce56
GAMING <stu2201321016@uni-plovdiv.bg>
2026-05-21 13:20:22 +0300

    Updated .gitignore

---
+++ School Warehouse/Migrations/20260521095713_AddUsersTableAndSeed.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace School_Warehouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsersTableAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "PasswordHash", "Role", "Username" },
+                values: new object[,]
+                {
+                    { 1, "AQAAAAIAAYagAAAAEFjGsPjizkVJeZdlyWOX8HttgVBjEro7zxJpm3gLX6k2+a3wpCgG2ri017OroKw+KA==", "admin", "admin" },
+                    { 2, "AQAAAAIAAYagAAAAEIS7EpWIO+EfTOZom4ThdH4Vjhr6DRGyEh0jxhjbkFAil9wHZGfVaACqdUVX7MoTPA==", "stationery", "stationery" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
